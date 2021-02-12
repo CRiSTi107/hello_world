e46e894f1e1b3e2a5895b7788a3078b65a6e1557
Cristian <cvoiculescu@icloud.com>
2021-02-13 00:33:23 +0200

    Added new user

--- Program.cs
+++ Program.cs
@@ -9,11 +9,13 @@ namespace hello_world
         {
             User user1 = new User();
             User user2 = new User(10, "CRiSTi107", "toor");
+            User user3 = new User(12, "Dobri01", "negro69");
 
             List<User> users = new List<User>();
 
             users.Add(user1);
             users.Add(user2);
+            users.Add(user3);
 
             foreach(User user in users)
             {
